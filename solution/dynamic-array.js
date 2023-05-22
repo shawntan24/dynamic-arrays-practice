20f8912b658fbb08ba039061475a3d143b3d6b2d
shawntan24 <shawntan24@yahoo.com>
2023-05-22 15:04:40 -0700

    completed

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -1,19 +1,35 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
-    // Your code here
+    this.data = new Array(defaultSize);
+    this.capacity = defaultSize;
+    this.length = 0;
 
   }
 
   read(index) {
 
-    // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
+    if(this.length === this.capacity){
+      const expandArr = new Array(newCapacity)
 
-    // Your code here
+      for (let i = 0; i < this.length; i++){
+        expandArr[i] = this.data[i];
+      }
+
+      this.data = expandArr;
+      this.capacity = newCapacity;
+    }
+
+    for (let i = this.length; i > 0; i--){
+      this.data[i] = this.data[i-1];
+    }
+    this.data[0] = val;
+    this.length++;
+
   }
 
 }
